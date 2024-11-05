07d9e703bc63b71cb87a6c4f959475031ed0dda5
HoodaHashem <mahmoudhashem335@gmail.com>
2024-11-06 00:20:48 +0200

    deleting some shit

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,11 +1,6 @@
 import { Server } from "socket.io";
 import "dotenv/config";
-import User from "../models/user.js";
 import socketHandler from "../socketHandlers/socketHandler.js";
-import getFriendRequests from "../socketHandlers/getFriendRequests.js";
-import FriendRequest from "../models/friendRequestsSchema.js";
-import broadcastFriendRequestUpdate from "../socketHandlers/broadcastFriendRequestUpdate.js";
-
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
